e59113583be51b60a1900eb0fce032a4083eec8e
Singyuen Yip <yeshengyuan@foxmail.com>
2017-05-01 17:37:19 +0800

    Add some thing.

---
+++ ImageMagickNetDemo/BitmapExtentions.cs
@@ -0,0 +1,118 @@
+﻿using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+
+namespace ImageMagickNetDemo
+{
+    public static class BitmapExtentions
+    {
+        public static Rgb[,] ToRgbArray(this Bitmap bitmap)
+        {
+            try
+            {
+                int height = bitmap.Height, width = bitmap.Width;
+                var rgbMatrix = new Rgb[height, width];
+                var rect = new Rectangle(0, 0, width, height);
+                var bmpData = bitmap.LockBits(rect, ImageLockMode.ReadWrite, bitmap.PixelFormat);
+                IntPtr iPtr = bmpData.Scan0;
+                var bytesCnt = height*width*3;
+                byte[] pixelValues = new byte[bytesCnt];
+                System.Runtime.InteropServices.Marshal.Copy(iPtr, pixelValues, 0, bytesCnt);
+                bitmap.UnlockBits(bmpData);
+                var pointIdx = 0;
+                for (var i = 0; i < height; ++i)
+                {
+                    for (var j = 0; j < width; ++j)
+                    {
+                        rgbMatrix[i, j].Red = pixelValues[pointIdx++];
+                        rgbMatrix[i, j].Green = pixelValues[pointIdx++];
+                        rgbMatrix[i, j].Blue = pixelValues[pointIdx++];
+                    }
+                }
+                return rgbMatrix;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        public static Rectangle FindBoundRectangle(this Bitmap bitmap)
+        {
+            var rgbs = bitmap.ToRgbArray();
+            int height = rgbs.GetLength(0), width = rgbs.GetLength(1);
+            int xCenter = width >> 1, yCenter = height >> 1;
+            int left = 0, right = width - 1, bottom = height - 1, top = 0;
+            for (var i = 0; i < xCenter; ++i)
+            {
+                if (rgbs[yCenter, i] == rgbs[yCenter, i + 1]) continue;
+                left = i;
+                break;
+            }
+            for (var j = width - 1; j > xCenter; --j)
+            {
+                if (rgbs[yCenter, j] == rgbs[yCenter, j - 1]) continue;
+                right = j;
+                break;
+            }
+            for (var k = 0; k < yCenter; ++k)
+            {
+                if (rgbs[k, xCenter] == rgbs[k + 1, xCenter]) continue;
+                top = k;
+                break;
+            }
+            for (var l = height - 1; l > yCenter; --l)
+            {
+                if (rgbs[l, xCenter] == rgbs[l - 1, xCenter]) continue;
+                bottom = l;
+                break;
+            }
+            var rect = new Rectangle(left, top, right - left + 1, bottom - top + 1);
+            return rect;
+        }
+    }
+
+    public struct Rgb
+    {
+        public bool Equals(Rgb other)
+        {
+            return Red == other.Red && Green == other.Green && Blue == other.Blue;
+        }
+
+        public override bool Equals(object obj)
+        {
+            if (ReferenceEquals(null, obj)) return false;
+            return obj is Rgb && Equals((Rgb) obj);
+        }
+
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = Red;
+                hashCode = (hashCode*397) ^ Green;
+                hashCode = (hashCode*397) ^ Blue;
+                return hashCode;
+            }
+        }
+
+        public int Red { get; set; }
+        public int Green { get; set; }
+        public int Blue { get; set; }
+
+        public static bool operator ==(Rgb left, Rgb right)
+        {
+            return left.Red == right.Red && left.Green == right.Green && left.Blue == right.Blue;
+        }
+
+        public static bool operator !=(Rgb left, Rgb right)
+        {
+            return !(left == right);
+        }
+
+        public override string ToString()
+        {
+            return $@"({Red}, {Green}, {Blue})";
+        }
+    }
+}
---
+++ ImageMagickNetDemo/Program.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Drawing;
+using System.IO;
+using GhostscriptSharp;
+using iTextSharp.text.pdf;
+using iTextSharp.text.pdf.parser;
+
+namespace ImageMagickNetDemo
+{
+    class Program
+    {
+        static void AddMarginRectangle(String src, String dest, Rectangle rect, int imgHeight, int dpi)
+        {
+            PdfReader reader = new PdfReader(src);
+            //PdfReaderContentParser parser = new PdfReaderContentParser(reader);
+            PdfStamper stamper = new PdfStamper(reader, new FileStream(dest, FileMode.OpenOrCreate));
+            for (int i = 1; i <= reader.NumberOfPages; i++)
+            {
+                //var finder = parser.ProcessContent(i, new TextMarginFinder());
+                PdfContentByte cb = stamper.GetOverContent(i);
+                //cb.Rectangle(finder.GetLlx(), finder.GetLly(),
+                //    finder.GetWidth(), finder.GetHeight());
+                cb.Rectangle(PixelsToPoints(rect.X, dpi), PixelsToPoints(imgHeight, dpi)
+                    - PixelsToPoints(rect.Y, dpi) - PixelsToPoints(rect.Height, dpi),
+                    PixelsToPoints(rect.Width, dpi), PixelsToPoints(rect.Height, dpi));
+                cb.Stroke();
+            }
+            stamper.Close();
+            reader.Close();
+        }
+
+        static void CropPdfWithRectangleAndHeight(string src, string dest, Rectangle rect, 
+            int imgHeight, int dpi)
+        {
+            var reader = new PdfReader(src);
+            var stamper = new PdfStamper(reader, new FileStream(dest, FileMode.OpenOrCreate));
+            float llx = PixelsToPoints(rect.X, dpi);
+            float lly = PixelsToPoints(imgHeight, dpi)
+                        - PixelsToPoints(rect.Y, dpi) - PixelsToPoints(rect.Height, dpi);
+            float urx = llx + PixelsToPoints(rect.Width, dpi);
+            float ury = lly + PixelsToPoints(rect.Height, dpi);
+            PdfRectangle pdfRectangle = new PdfRectangle(llx, lly, urx, ury);
+            for (var i = 1; i <= reader.NumberOfPages; ++i)
+            {
+                var pageDict = reader.GetPageN(i);
+                pageDict.Put(PdfName.CROPBOX, pdfRectangle);
+            }
+            stamper.Close();
+            reader.Close();
+        }
+
+        public static float PixelsToPoints(float value, int dpi)
+        {
+            return value / dpi * 72;
+        }
+
+        static void Main(string[] args)
+        {
+            var pdfFilePath = Environment.CurrentDirectory + @"\IR reader new Schematic.pdf";
+            var imgFilePath = Environment.CurrentDirectory + @"\IR reader new Schematic.jpg";
+
+            GhostscriptWrapper.GeneratePageThumb(pdfFilePath, imgFilePath, 1, 300, 300);
+
+            Bitmap bitmap = new Bitmap(imgFilePath);
+            //Rgb[,] rgbs = bitmap.ToRgbArray();
+            //for (var i = 0; i < rgbs.GetLength(0); ++i)
+            //{
+            //    for (var j = 0; j < rgbs.GetLength(1); ++j)
+            //    {
+            //        Console.WriteLine(rgbs[i, j]);
+            //    }
+            //}
+            Console.WriteLine($@"Height: {bitmap.Height}, Width: {bitmap.Width}");
+            Console.WriteLine(bitmap.FindBoundRectangle());
+            AddMarginRectangle(Environment.CurrentDirectory + @"\IR reader new Schematic.pdf", 
+                Environment.CurrentDirectory + @"\IR reader new Schematic mark.pdf",
+                bitmap.FindBoundRectangle(), bitmap.Height, 300);
+            CropPdfWithRectangleAndHeight(Environment.CurrentDirectory + @"\IR reader new Schematic.pdf",
+                Environment.CurrentDirectory + @"\IR reader new Schematic Crop.pdf",
+                bitmap.FindBoundRectangle(), bitmap.Height, 300);
+        }
+    }
+}
